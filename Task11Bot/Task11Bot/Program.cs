8e6f4f5872efef2a3d773619b98f9b130fc35af2
profos1983 <jianko@hotmail.com>
2025-12-21 21:55:25 +0300

    2025-12-21 часть1

---
+++ Task11Bot/Task11Bot/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using Task11Bot.VoiceTexterBot;
+using Telegram.Bot;
+
+namespace Task11Bot
+{
+    public class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // Объект, отвечающий за постоянный жизненный цикл приложения
+            var host = new HostBuilder()
+               .ConfigureServices((hostContext, services) => ConfigureServices(services)) // Задаем конфигурацию
+                .UseConsoleLifetime() // Позволяет поддерживать приложение активным в консоли
+                .Build(); // Собираем
+
+            Console.WriteLine("Сервис запущен");
+            // Запускаем сервис
+            await host.RunAsync();
+            Console.WriteLine("Сервис остановлен");
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {
+            // Регистрируем объект TelegramBotClient c токеном подключения
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("8456289298:AAH73shDlG0HOzYlJ2DW1baiGPJJEnILnKU"));
+            // Регистрируем постоянно активный сервис бота
+            services.AddHostedService<Bot>();
+        }
+
+    }
+}
+
+//Остановился на начале листа 11.3
